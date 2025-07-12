76b07cb3a02154c4c7fd2e80bd0481a14c3f5fe6
Vinicius Ramos Silva Filho <vinicius.filho@cwi.com.br>
2025-07-12 15:51:50 -0300

    feat: initial commit

---
+++ src/main/java/com/fiap/soat12/tc_group_7/TcGroup7Application.java
@@ -0,0 +1,13 @@
+package com.fiap.soat12.tc_group_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcGroup7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcGroup7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/soat12/tc_group_7/TcGroup7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.soat12.tc_group_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcGroup7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
