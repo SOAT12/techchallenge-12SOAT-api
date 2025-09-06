86c6561211f874e4792065fc319daf01a4263dc6
caiohnrq <caio_henrique.16@hotmail.com>
2025-09-06 13:06:37 -0300

    feat: adapting the Stock and ToolCategory to clean arch

---
+++ src/main/java/com/fiap/soat12/tc_group_7/api/application/port/in/CreateStockItemUseCase.java
@@ -0,0 +1,37 @@
+package com.fiap.soat12.tc_group_7.api.application.port.in;
+
+import com.fiap.soat12.tc_group_7.api.domain.model.Stock;
+import com.fiap.soat12.tc_group_7.api.domain.model.ToolCategory;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public interface CreateStockItemUseCase {
+
+    Stock createStock(CreateStockCommand stock);
+
+    record CreateStockCommand(
+            @NotBlank(message = "The toolname cannot be blank.")
+            String toolName,
+
+            @NotNull(message = "The value cannot be null.")
+            @Min(value = 0L, message = "The value cannot be fewer than zero.")
+            BigDecimal value,
+
+            @NotNull(message = "The quantity cannot be null.")
+            @Min(value = 0, message = "The quantity cannot be fewer than zero.")
+            Integer quantity,
+
+            @NotNull(message = "The toolCategory cannot be null.")
+            ToolCategory toolCategory
+    ) {
+
+        public CreateStockCommand {
+            if (toolName != null && toolName.trim().isEmpty()) {
+                throw new IllegalArgumentException("First name cannot be only whitespace.");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/fiap/soat12/tc_group_7/api/application/useCases/CreateStockItemUseCaseImpl.java
@@ -0,0 +1,11 @@
+package com.fiap.soat12.tc_group_7.api.application.useCases;
+
+import com.fiap.soat12.tc_group_7.api.application.port.in.CreateStockItemUseCase;
+import com.fiap.soat12.tc_group_7.api.domain.model.Stock;
+
+public class CreateStockItemUseCaseImpl implements CreateStockItemUseCase {
+
+    public Stock createStock(CreateStockCommand command) {
+        return null;
+    }
+}
