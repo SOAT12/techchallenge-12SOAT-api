4287032f2581d833dc35f5cf3ef6d6cd42980297
AlexPires1 <alexpirespereira747@gmail.com>
2026-01-06 21:19:56 -0300

    feat: adicionando novas envs no properties

--- src/main/java/com/fiap/soat12/tc_group_7/TcGroup7Application.java
+++ src/main/java/com/fiap/soat12/tc_group_7/TcGroup7Application.java
@@ -1,10 +1,7 @@
 package com.fiap.soat12.tc_group_7;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -15,10 +12,4 @@ public class TcGroup7Application {
 		SpringApplication.run(TcGroup7Application.class, args);
 	}
 
-    @Bean
-    ApplicationRunner applicationRunner(@Value("${SPRING_DATASOURCE_USERNAME}") String secret) {
-        return args -> {
-            System.out.println("SECRET FROM AWS LOADED: " + secret);
-        };
-    }
 }
